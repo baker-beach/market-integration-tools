b22561c67f5a56883d8fe175ad3a2aa26312f3bb
achimwolf <achim.wolf@baker-beach.com>
2017-04-20 18:08:44 +0200

    tmp

--- src/main/java/com/bakerbeach/market/integration/util/TranslationImportUtil.java
+++ src/main/java/com/bakerbeach/market/integration/util/TranslationImportUtil.java
@@ -11,14 +11,39 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bakerbeach.market.core.service.I18NMessageImpl;
+import com.bakerbeach.market.translation.api.model.I18NMessage;
+
 import au.com.bytecode.opencsv.CSVReader;
 
-public class ProductImportUtil {
-	protected static final Logger log = LoggerFactory.getLogger(ProductImportUtil.class);
+public class TranslationImportUtil {
+	protected static final Logger log = LoggerFactory.getLogger(TranslationImportUtil.class);
+	
+	public static List<I18NMessage> getTranslations(List<Map<String, String>> list) {
+		List<I18NMessage> messages = new ArrayList<>();
+		
+		for(Map<String, String> map : list) {
+			try {
+				I18NMessageImpl message = new I18NMessageImpl();
+				message.setMessageKey(map.get("code"));
+				message.setTag(map.get("tag"));
+				message.setType(map.get("type"));
+				
+				message.getMessages().put("de", map.get("de"));
+				message.getMessages().put("en", map.get("en"));
+				
+				messages.add(message);				
+			} catch (Exception e) {
+				e.getStackTrace();
+			}				
+		}
+		
+		return messages;
+	}
 	
 	public static List<Map<String, String>> readCsv(String path) {		
 		try {
-			CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(path)), ',', '\"', CSVReader.DEFAULT_ESCAPE_CHARACTER);
+			CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(path), "UTF-8"), ';', '\"', CSVReader.DEFAULT_ESCAPE_CHARACTER);
 			List<String[]> in = reader.readAll();
 			reader.close();
 			
@@ -50,5 +75,6 @@ public class ProductImportUtil {
 		}
 		return result;
 	}
-	
+
+
 }
